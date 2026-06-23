13fb023c6fa998c9ccfd242e65aa5c5b098016e3
Wiktor <voxxorion@gmail.com>
2026-06-24 00:06:25 +0200

    clear text

--- src/backend/main_api.py
+++ src/backend/main_api.py
@@ -1,20 +1,3 @@
-# from fastapi import FastAPI, HTTPException
-# from deckhand import deckhand
-
-# app = FastAPI()
-
-# @app.post("/deckhand")
-# def deckh_endpoint(payload: dict):
-
-#     if "imo" not in payload:
-#         raise HTTPException(
-#             status_code=400,
-#             detail="IMO is required"
-#         )
-
-#     return deckhand(payload)
-
-
 from fastapi import FastAPI, HTTPException
 from deckhand import deckhand
 

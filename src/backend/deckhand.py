cd08a1bbb2708c7a6b296da3740724c11b8aa14c
Wiktor <voxxorion@gmail.com>
2026-06-23 22:57:53 +0200

    repaired json

--- src/backend/deckhand.py
+++ src/backend/deckhand.py
@@ -5,23 +5,21 @@ FIELD_MAPPING = {
     "ship_width": "breadth",
     "ship_length": "length",
     "ship_submersion": "draft",
-    "ship_weigth": "grossTonnage",
+    "ship_weight": "grossTonnage",
 }
 
+def deckhand(imo: str) -> dict:
 
-def deckhand(ship_data: dict) -> dict:
-    imo = ship_data["imo"]
     api_response = get_ship_by_imo(imo)
+
     vessels = api_response.get("vessels", [])
+
     if not vessels:
-        return ship_data
+        return {}
+
     vessel = vessels[0]
 
-    for local_field, api_field in FIELD_MAPPING.items():
-        current_value = ship_data.get(local_field)
-        if current_value in [None, ""]:
-            api_value = vessel.get(api_field)
-            if api_value is not None:
-                ship_data[local_field] = api_value
-                
-    return ship_data
+    return {
+        local_field: vessel.get(api_field)
+        for local_field, api_field in FIELD_MAPPING.items()
+    }
--- src/backend/main_api.py
+++ src/backend/main_api.py
@@ -1,15 +1,33 @@
+# from fastapi import FastAPI, HTTPException
+# from deckhand import deckhand
+
+# app = FastAPI()
+
+# @app.post("/deckhand")
+# def deckh_endpoint(payload: dict):
+
+#     if "imo" not in payload:
+#         raise HTTPException(
+#             status_code=400,
+#             detail="IMO is required"
+#         )
+
+#     return deckhand(payload)
+
+
 from fastapi import FastAPI, HTTPException
 from deckhand import deckhand
 
 app = FastAPI()
 
-@app.post("/deckhand")
-def deckh_endpoint(payload: dict):
+@app.get("/deckhand")
+def deckhand_endpoint(imo: str):
 
-    if "imo" not in payload:
+    result = deckhand(imo)
+
+    if not result:
         raise HTTPException(
-            status_code=400,
-            detail="IMO is required"
+            status_code=404,
+            detail=f"Ship with IMO {imo} not found"
         )
-
-    return deckhand(payload)
+    return result

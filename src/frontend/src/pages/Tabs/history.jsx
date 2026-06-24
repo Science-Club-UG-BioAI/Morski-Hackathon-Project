21282be1d5518245a198109cd62a95095710b6e0
voxorion <voxxorion@gmail.com>
2026-06-24 09:42:36 +0200

    change to the src

--- src/frontend/src/pages/Tabs/history.jsx
+++ src/frontend/src/pages/Tabs/history.jsx
@@ -1,11 +1,23 @@
+import data from "../../../model/save/2.json";
 
-
-
-export default function History () {
+export default function History() {
     return (
         <div className="page page-history-private">
             <h1>History Panel</h1>
-            <p>Logged as: {UserActivation.login}</p>
+
+            <div className="history-card">
+                <h3>ID: {data.id}</h3>
+
+                <p>
+                    <strong>Ship Name:</strong>{" "}
+                    {data.task["Fill PCS form"]["Ship Name"]}
+                </p>
+
+                <p>
+                    <strong>Ship IMO number:</strong>{" "}
+                    {data.task["Fill PCS form"]["Ship IMO number"]}
+                </p>
+            </div>
         </div>
     );
 }
